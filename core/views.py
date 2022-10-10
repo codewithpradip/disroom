ae013f4200594871477bbfd06e28cf407fac9a1c
Pradip Thapa <pradipthapaofficial@gmail.com>
2022-10-10 21:37:42 +0545

    feat: implement room search feature

--- core/views.py
+++ core/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from room.models import Room,Topic
+from django.db.models import Q
 
 # Create your views here.
 def home_view(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
     topics = Topic.objects.all()
     room_count = rooms.count()
     context = {'rooms': rooms, 'room_count': room_count, 'topics': topics}
