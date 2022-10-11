3090c9f2dfb196251ffe2e6c1102691e0188ab6e
Pradip Thapa <pradipthapaofficial@gmail.com>
2022-10-11 13:29:41 +0545

    feat: implement user registration

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('user-login/', views.user_login_view, name='user_login'),
     path('user-logout/', views.user_logout_view, name='user_logout'),
+    path('user-register/', views.user_register_view, name='user_register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def user_login_view(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     if request.method == 'POST':
@@ -15,9 +17,25 @@ def user_login_view(request):
             return redirect('home')
         else:
             messages.error(request, 'User OR Password does not match.')
-    return render(request, 'account/login_register.html')
+    context = {'page': page}
+    return render(request, 'account/login_register.html', context)
 
 
 def user_logout_view(request):
     logout(request)
     return redirect('home')
+
+
+def user_register_view(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration.')
+    return render(request, 'account/login_register.html', {'form': form})
