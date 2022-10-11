d368d8e96a3ec04de40d7f8e8510f29ee2c01438
Pradip Thapa <pradipthapaofficial@gmail.com>
2022-10-11 21:01:47 +0545

    feat: add user profile section

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('user-login/', views.user_login_view, name='user_login'),
     path('user-logout/', views.user_logout_view, name='user_logout'),
     path('user-register/', views.user_register_view, name='user_register'),
+    path('user-profile/<int:pk>', views.user_profile_view, name='user_profile'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from room.models import Room, Message, Topic
 
 # Create your views here.
 def user_login_view(request):
@@ -39,3 +41,12 @@ def user_register_view(request):
         else:
             messages.error(request, 'An error occurred during registration.')
     return render(request, 'account/login_register.html', {'form': form})
+
+
+def user_profile_view(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user,  'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
+    return render(request, 'account/user_profile.html', context)
