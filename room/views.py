aa0398dd921cd3d01490f85109ba10200e205b3d
Pradip Thapa <pradipthapaofficial@gmail.com>
2022-10-11 10:54:30 +0545

    fix: restrict not login user to access restricted pages

--- room/views.py
+++ room/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Room
 from .forms import RoomForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required(login_url='user_login')
 def room_detail_view(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'room/room_detail.html', context)
 
+
+@login_required(login_url='user_login')
 def create_room_view(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -18,6 +23,8 @@ def create_room_view(request):
     context = {'form': form}
     return render(request, 'room/create_room_form.html', context)
 
+
+@login_required(login_url='user_login')
 def update_room_view(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
@@ -30,6 +37,7 @@ def update_room_view(request, pk):
     return render(request, 'room/create_room_form.html', context)
 
 
+@login_required(login_url='user_login')
 def delete_room_view(request, pk):
     room = Room.objects.get(id=pk)
     if request.method == 'POST':
