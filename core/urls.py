71c46a22746d5ad6a0bb57aad88fe8bc06c80010
Pradip Thapa <pradipthapaofficial@gmail.com>
2022-10-13 22:21:12 +0545

    refactor: add topic, activity page for mobile responsive

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('topics/', views.topics_page_view, name='topics'),
+    path('activity/', views.activity_paga_view, name='activity'),
 ]
--- core/views.py
+++ core/views.py
@@ -11,7 +11,18 @@ def home_view(request):
         Q(description__icontains=q)
     )
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     context = {'rooms': rooms, 'room_count': room_count, 'topics': topics, 'room_messages': room_messages}
     return render(request, 'core/home.html', context)
+
+def topics_page_view(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {'topics' : topics}
+    return render(request, 'core/mobile_topics.html', context)
+
+def activity_paga_view(request):
+    room_messages = Message.objects.all()
+    context = {'room_messages': room_messages}
+    return render(request, 'core/mobile_activity.html', context)
